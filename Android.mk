b67cd21adc516773cca76eeb106b1369a90fe723
Paul Keith <javelinanddart@gmail.com>
2018-02-21 01:56:45 +0100

    vold: Make exfat driver support generic
    * Samsung moved to a new driver in their recent devices,
      and inevitably there will be more in the future
    * Make our support generic with the flag TARGET_EXFAT_DRIVER=foo,
      where foo is the -t arg the fs needs to be mounted with
    
    Change-Id: I984481972bf79bf195321e69906cc5994fb19f2d

--- Android.mk
+++ Android.mk
@@ -92,8 +92,8 @@ ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
 vold_cflags += -DCONFIG_KERNEL_HAVE_EXFAT
 endif
 
-ifeq ($(TARGET_KERNEL_HAVE_NTFS),true)
-vold_cflags += -DCONFIG_KERNEL_HAVE_NTFS
+ifneq ($(TARGET_EXFAT_DRIVER),)
+  vold_cflags += -DCONFIG_EXFAT_DRIVER=\"$(TARGET_EXFAT_DRIVER)\"
 endif
 
 include $(CLEAR_VARS)
