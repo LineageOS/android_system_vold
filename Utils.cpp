7db856f3a367b3c0f649ec39f858a845d2d5abc5
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-25 18:36:36 +0100

    Add exfat to supported FSs if exfat is enabled
    * Remove fuse exfat while we're at it
    
    Change-Id: Id6b9ff5d480f7eb1cd7fe6f93db8678aabf83f44

--- Utils.cpp
+++ Utils.cpp
@@ -547,8 +547,13 @@ bool IsFilesystemSupported(const std::string& fsType) {
     }
 
     /* fuse filesystems */
-    supported.append("fuse\tntfs\n"
-                     "fuse\texfat\n");
+    supported.append("fuse\tntfs\n");
+
+#ifdef CONFIG_EXFAT_DRIVER
+    /* Add exfat if an exfat driver is present */
+    if (supported.find(CONFIG_EXFAT_DRIVER "\n") != std::string::npos)
+        supported.append("nodev\texfat\n");
+#endif
 
     return supported.find(fsType + "\n") != std::string::npos;
 }
