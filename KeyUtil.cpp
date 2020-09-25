3ef3b35a49e8a82c2b74e9018abe531800bf1188
Pig <pig.priv@gmail.com>
2020-09-25 22:56:33 +0800

    vold: Bring in more wrapped key changes
    Conflicts:
    	KeyStorage.cpp
    	KeyUtil.cpp
    
    [wight554: Apply changes from CAF 12]
    [StudioKeys: Apply changes from LA.QSSI.17.0.r1-08000-qssi.0]
    
    Change-Id: I44e81afaec78c567a0bf2eed30a79eb737e2a867
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- KeyUtil.cpp
+++ KeyUtil.cpp
@@ -267,7 +267,10 @@ bool installKey(const std::string& mountpoint, const EncryptionOptions& options,
             // A key for a v1 policy is specified by an arbitrary 8-byte
             // "descriptor", which must be provided by userspace.  We use the
             // first 8 bytes from the double SHA-512 of the key itself.
-            policy->key_raw_ref = generateKeyRef((const uint8_t*)key.data(), key.size());
+            /* When wrapped key is supported, only the first 32 bytes are
+                the same per boot. The second 32 bytes can change as the ephemeral
+                key is different. */
+            policy->key_raw_ref = generateKeyRef((const uint8_t*)key.data(), key.size()/2);
             if (!buildKeySpecifier(&arg->key_spec, *policy)) {
                 return false;
             }
