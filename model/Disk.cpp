LineageOS/android_system_vold

commit ad5b4f9c6bc7db7622791ac9a24d7eadcb60d714

vold: skip first disk change when converting MBR to GPT
When converting a public (MBR) partition to private (GPT) partition,
'sgdisk --zap-all <path>' triggers a disk change netlink event
when converting from MBR to GPT.  Then, 'sgdisk --new=....' triggers
another disk change netlink event.

vold informs clients a new volume is created after the first disk
change event occurs.  system server reacts by requesting to mount
the volume.  If this request is honored before the second disk change
event, the volume will be unmounted immediately after system server's
request to mount is honored.  The next time system server performs
an operation (createnewuser) on this volume, it will fail due to
the volume being unmounted.

This is reproduced by running the following commands in a loop:

adb shell sm partition <disk> private
adb shell sm partition <disk> public
adb shell sm forget all

OR

run cts -c com.android.cts.appsecurity.AdoptableHostTest -m testPackageInstaller

This change causes vold to delay notifying clients that the volume is
ready until after it's actually partitioned.

CYNGNOS-2283
Change-Id: I457cc1508573d73ef2be2f0cfdc5c2237bfabad7

model/Disk.cpp
@@ -97,7 +97,8 @@ Disk::Disk(const std::string& eventPath, dev_t device, const std::string& nickna
       mNickname(nickname),
       mFlags(flags),
       mCreated(false),
-      mJustPartitioned(false) {
+      mJustPartitioned(false),
+      mSkipChange(false) {
     mId = StringPrintf("disk:%u,%u", major(device), minor(device));
     mEventPath = eventPath;
     mSysPath = StringPrintf("/sys/%s", eventPath.c_str());
@@ -235,6 +236,11 @@ void Disk::destroyAllVolumes() {
 }
 
 status_t Disk::readMetadata() {
+
+    if (mSkipChange) {
+        return OK;
+    }
+
     mSize = -1;
     mLabel.clear();
 
@@ -324,6 +330,12 @@ status_t Disk::readPartitions() {
     int maxMinors = getMaxMinors();
     if (maxMinors < 0) {
         return -ENOTSUP;
+    }
+
+    if (mSkipChange) {
+        mSkipChange = false;
+        LOG(INFO) << "Skip first change";
+        return OK;
     }
 
     destroyAllVolumes();
@@ -446,8 +458,45 @@ status_t Disk::partitionPublic() {
     destroyAllVolumes();
     mJustPartitioned = true;
 
-    // First nuke any existing partition table
+    // Determine if we're coming from MBR
     std::vector<std::string> cmd;
+    cmd.push_back(kSgdiskPath);
+    cmd.push_back("--android-dump");
+    cmd.push_back(mDevPath);
+
+    std::vector<std::string> output;
+    res = ForkExecvp(cmd, &output);
+    if (res != OK) {
+        LOG(WARNING) << "sgdisk failed to scan " << mDevPath;
+        mJustPartitioned = false;
+        return res;
+    }
+
+    Table table = Table::kUnknown;
+    for (auto line : output) {
+        char* cline = (char*) line.c_str();
+        char* token = strtok(cline, kSgdiskToken);
+        if (token == nullptr) continue;
+
+        if (!strcmp(token, "DISK")) {
+            const char* type = strtok(nullptr, kSgdiskToken);
+            if (!strcmp(type, "mbr")) {
+                table = Table::kMbr;
+                break;
+            } else if (!strcmp(type, "gpt")) {
+                table = Table::kGpt;
+                break;
+            }
+        }
+    }
+
+    if (table == Table::kMbr) {
+        LOG(INFO) << "skip first disk change event due to MBR -> GPT switch";
+        mSkipChange = true;
+    }
+
+    // First nuke any existing partition table
+    cmd.clear();
     cmd.push_back(kSgdiskPath);
     cmd.push_back("--zap-all");
     cmd.push_back(mDevPath);

model/Disk.h
@@ -116,6 +116,8 @@ class Disk {
     bool mCreated;
     /* Flag that we just partitioned and should format all volumes */
     bool mJustPartitioned;
+    /* Flag that we need to skip first disk change events after partitioning*/
+    bool mSkipChange;
 
     void createPublicVolume(dev_t device);
     void createPrivateVolume(dev_t device, const std::string& partGuid);

commit 2711bf7059318aa6a61e37392a0aee6263bff0a0

vold: fix failing to format zero-ed out SD card
sgdisk --android-dump fails with error code 8 when there is no partition
table. Don't stop us from creating a new partition table.

Can be reproduced with
1. adb shell sm set-virtual-disk true
2. Format from UI
3. Observe fake "success" message with SD still unformatted

Fixes: ad5b4f9 ("vold: skip first disk change when converting MBR to GPT")
Test: CtsOsTestCases android.os.storage.cts.StorageManagerTest#testGetStorageVolumeSDCard
Change-Id: I3262bb2e28de438beba541a97038a06432643a65

model/Disk.cpp
@@ -471,26 +471,23 @@ status_t Disk::partitionPublic() {
 
     std::vector<std::string> output;
     res = ForkExecvp(cmd, &output);
-    if (res != OK) {
-        LOG(WARNING) << "sgdisk failed to scan " << mDevPath;
-        mJustPartitioned = false;
-        return res;
-    }
-
     Table table = Table::kUnknown;
-    for (auto line : output) {
-        char* cline = (char*) line.c_str();
-        char* token = strtok(cline, kSgdiskToken);
-        if (token == nullptr) continue;
+    // fails when there is no partition table, it's okay
+    if (res == OK) {
+        for (auto line : output) {
+            char* cline = (char*) line.c_str();
+            char* token = strtok(cline, kSgdiskToken);
+            if (token == nullptr) continue;
 
-        if (!strcmp(token, "DISK")) {
-            const char* type = strtok(nullptr, kSgdiskToken);
-            if (!strcmp(type, "mbr")) {
-                table = Table::kMbr;
-                break;
-            } else if (!strcmp(type, "gpt")) {
-                table = Table::kGpt;
-                break;
+            if (!strcmp(token, "DISK")) {
+                const char* type = strtok(nullptr, kSgdiskToken);
+                if (!strcmp(type, "mbr")) {
+                    table = Table::kMbr;
+                    break;
+                } else if (!strcmp(type, "gpt")) {
+                    table = Table::kGpt;
+                    break;
+                }
             }
         }
     }